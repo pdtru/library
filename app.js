b442dee6a146ee8e9300f7367f98e3be1d2278ca
Peter Tran-Truong <peter.dai.truong@gmail.com>
2023-05-04 16:48:30 -0700

    Add library class

--- app.js
+++ app.js
@@ -1,6 +1,46 @@
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
+}
+
+class Library {
+  constructor() {
+    this.Books = new Map();
+  }
+  addBook = function (book) {
+    this.Books.set(book.title, book);
+  };
+
+  removeBook = function (title) {
+    this.Books.delete(title);
+  };
+}
+
+const myLibrary = new Library();
+
+const addBookButton = document.getElementById('add-book-button');
 const openButton = document.querySelector('[data-open-modal]');
 const closeButton = document.querySelector('[data-close-modal]');
 const modal = document.querySelector('[data-modal]');
+const title = document.getElementById('title');
+const author = document.getElementById('author');
+const pages = document.getElementById('pages');
+const read = document.getElementById('read');
+
+addBookButton.addEventListener('click', () => {
+  const book = new Book(title.value, author.value, pages.value, read.value);
+  myLibrary.addBook(book);
+  console.log(myLibrary.Books.get(title.value));
+  title.value = null;
+  author.value = null;
+  pages.value = null;
+  read.value = false;
+  modal.close();
+});
 
 openButton.addEventListener('click', () => {
   modal.showModal();
